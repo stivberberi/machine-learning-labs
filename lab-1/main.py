04576051c510ab8be489575a36fe3f976027747f
stivberberi <stiv.berberi1@gmail.com>
2022-10-14 13:29:07 -0400

    Generating weights working

--- lab-1/main.py
+++ lab-1/main.py
@@ -8,7 +8,8 @@ def generate_X_m(M: int, x_train):
     appended to the end of a ones column.
     '''
     X_m = np.ones((len(x_train), 1))    # column of ones
-    x_train = np.array([x_train])     # add [] to make 2D array (transposable)
+    # add [] to make 2D array (so it's transposable)
+    x_train = np.array([x_train])
     for m in range(1, M+1):
         x_train_m = np.power(x_train.T, m)
         X_m = np.append(X_m, x_train_m, axis=1)
@@ -16,7 +17,27 @@ def generate_X_m(M: int, x_train):
     return X_m
 
 
-def train_w()
+def train_w(X, t):
+    '''
+    Calculate the weights when training a set of examples for a given target set
+    '''
+    num_rows, num_col = np.shape(X)  # get the number of examples
+    XTX = np.dot(X.T, X)
+    if np.linalg.det(XTX) == 0:
+        w = np.zeros((num_rows, 1))
+    else:
+        w = (np.linalg.inv(XTX)) * np.dot(X.T, t)
+
+    return w
+
+
+def calc_error(X, w, t):
+    '''
+    Calculate the error (training or validation) based on given training data, weights,
+    and a target set (training or validation)
+    '''
+    num_rows, num_col = np.shape(X)  # get the number of examples
+    error = (1 / num_col) * (X*w - t).T * (X*w - t)
 
 
 def main():
@@ -28,25 +49,25 @@ def main():
     t_train = np.sin(4*np.pi*X_train) + 0.3 * np.random.randn(10)
 
     '''
-    Store each training matrix of size M + 1 into a dict
+    Store each training matrix and corresponding weights into a dict
     '''
-    X_m = {
-        'M0': None,
-        'M1': None,
-        'M2': None,
-        'M3': None,
-        'M4': None,
-        'M5': None,
-        'M6': None,
-        'M7': None,
-        'M8': None,
-        'M9': None,
+    models = {
+        'M0': {'X_m': None, 'w': None},
+        'M1': {'X_m': None, 'w': None},
+        'M2': {'X_m': None, 'w': None},
+        'M3': {'X_m': None, 'w': None},
+        'M4': {'X_m': None, 'w': None},
+        'M5': {'X_m': None, 'w': None},
+        'M6': {'X_m': None, 'w': None},
+        'M7': {'X_m': None, 'w': None},
+        'M8': {'X_m': None, 'w': None},
+        'M9': {'X_m': None, 'w': None},
     }
     i = 0
-    for m in X_m:
-        X_m[m] = generate_X_m(i, X_train)
+    for m in models:
+        models[m]['X_m'] = generate_X_m(i, X_train)
         i += 1
-    print(X_m)
+        models[m]['w'] = train_w(models[m]['X_m'], t_train)
 
 
 main()
