75f8c9a09127a301be653391cd5a71f6320d70c1
stivberberi <stiv.berberi1@gmail.com>
2023-04-09 00:35:14 -0400

    need to adapt patch generation to simulate bayer patch

--- demosaicing_algorithm/linear_regresssion.py
+++ demosaicing_algorithm/linear_regresssion.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.ndimage import generic_filter
 
 
 def generate_patch(size: int, image: np.ndarray, x: int, y: int):
@@ -31,42 +30,53 @@ def generate_training_data(images: list):
         tuple: Tuple of 8 np arrays containing the x and y matrices for each patch type.
     """
 
-    # Define all X matrices (8)
+    # Define all X matrices (8) as empty 2d np matrices
     # naming scheme is x_<pixel> where pixel is the color of the pixel
     # ex. for green, is either x_gb or x_gr depending on the row (blue or red row)
-    x_gb, x_gr, x_r, x_b = [], [], [], []
+    x_gb, x_gr, x_r, x_b = np.empty((1, 25)), np.empty(
+        (1, 25)), np.empty((1, 25)), np.empty((1, 25))
 
     # Same for y matrices
-    y_gb, y_gr, y_r, y_b = [], [], [], []
+    y_gb, y_gr, y_r, y_b = np.empty((1, 1)), np.empty(
+        (1, 1)), np.empty((1, 1)), np.empty((1, 1))
 
     for image in images:
+        print(image.shape)
         # loop through x,y coords but leave 2 pixels on each side for padding
         for i in range(2, image.shape[0] - 2):
             for j in range(2, image.shape[1] - 2):
                 # generate patches of size 5x5
                 patch = generate_patch(5, image, i, j)
+                flat_patch = np.reshape(patch, (1, 25))
+                print(flat_patch.shape)
 
                 # 4 patch cases based on pixel location and rggb bayer pattern
                 if i % 2 == 0 and j % 2 == 0:
                     # both even, green pixel in blue row
-                    x_gb.append(patch.flatten())
-                    y_gb.append(image[i, j])
+                    np.append(x_gb, flat_patch, axis=0)
+                    np.append(y_gb, image[i, j, 1], axis=0)
+
+                    print(x_gb.shape)
+                    print(y_gb.shape)
+                    return
 
                 if i % 2 == 1 and j % 2 == 1:
                     # both odd, green pixel in red row
-                    x_gr.append(patch.flatten())
-                    y_gr.append(image[i, j])
+                    np.append(x_gr, patch.flatten())
+                    np.append(y_gr, image[i, j])
 
                 if i % 2 == 0 and j % 2 == 1:
                     # even row, odd column, blue pixel
-                    x_b.append(patch.flatten())
-                    y_b.append(image[i, j])
+                    np.append(x_b, patch.flatten())
+                    np.append(y_b, image[i, j])
 
                 if i % 2 == 1 and j % 2 == 0:
                     # odd row, even column, red pixel
-                    x_r.append(patch.flatten())
-                    y_r.append(image[i, j])
+                    np.append(x_r, patch.flatten())
+                    np.append(y_r, image[i, j])
 
+    print('hi')
+    print(x_b.shape)
     return x_gb, x_gr, x_r, x_b, y_gb, y_gr, y_r, y_b
 
 
@@ -77,9 +87,35 @@ def train_model(images: list):
         images (list): List of training images.
 
     Returns:
-        LinearRegression: Linear regression model.
+        tuple: Tuple of 8 np arrays containing the coefficients for each patch type.
     """
     # generate training data
     x_gb, x_gr, x_r, x_b, y_gb, y_gr, y_r, y_b = generate_training_data(images)
 
     # get coefficient matrices (8) using linear regression
+    # naming scheme is a_<pixel>_<source> where pixel is the colour it is trying to predict and source is the X matrix
+    # green predictor coefficients
+    a_g_r = np.matmul(np.linalg.inv(np.matmul(x_r.T, x_r)),
+                      np.matmul(x_r.T, y_r[:, :, 1]))
+    a_g_r = np.linalg.lstsq(x_r, y_r[:, :, 1], rcond=None)[0]
+    a_g_b = np.linalg.lstsq(x_b, y_b[:, :, 1], rcond=None)[0]
+
+    # blue predictor coefficients
+    a_b_gb = np.linalg.lstsq(x_gb, y_gb[:, :, 2], rcond=None)[0]
+    a_b_gr = np.linalg.lstsq(x_gr, y_gr[:, :, 2], rcond=None)[0]
+    a_b_r = np.linalg.lstsq(x_r, y_r[:, :, 2], rcond=None)[0]
+
+    # red predictor coefficients
+    a_r_gb = np.linalg.lstsq(x_gb, y_gb[:, :, 0], rcond=None)[0]
+    a_r_gr = np.linalg.lstsq(x_gr, y_gr[:, :, 0], rcond=None)[0]
+    a_r_b = np.linalg.lstsq(x_b, y_b[:, :, 0], rcond=None)[0]
+
+    # write coefficients as columns in a csv file with variable as header
+    with open('coefficients.csv', 'w') as f:
+        f.write('a_g_r,a_g_b,a_b_gb,a_b_gr,a_b_r,a_r_gb,a_r_gr,a_r_b\n')
+        for i in range(25):
+            f.write(
+                f'{a_g_r[i]},{a_g_b[i]},{a_b_gb[i]},{a_b_gr[i]},{a_b_r[i]},{a_r_gb[i]},{a_r_gr[i]},{a_r_b[i]}\n')
+
+    model = (a_g_r, a_g_b, a_b_gb, a_b_gr, a_b_r, a_r_gb, a_r_gr, a_r_b)
+    return model
--- demosaicing_algorithm/main.py
+++ demosaicing_algorithm/main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from linear_interpolation import demosaic_linear_interpolation
+from linear_regresssion import train_model, generate_patch
 
 
 def load_training_images():
@@ -11,23 +12,78 @@ def load_training_images():
     # images taken from McMaster dataset: https://www4.comp.polyu.edu.hk/~cslzhang/CDM_Dataset.htm
     # images are 512x512 pixels with 3 channels (RGB)
     # image = Image.open('McM/1.tif')
-    image = Image.open('test_images/test1.png')
+    image = Image.open('McM/1.tif')
     return image
+
+
+def demosaic_image(image, model):
+    """Demosaics the image using coeeficients from the linear regression model.
+    """
+
+    # expand model matrixes
+    a_g_r, a_g_b, a_b_gb, a_b_gr, a_b_r, a_r_gb, a_r_gr, a_r_b = model
+
+    # initiate array of same shape as image but with 3 channels
+    demosaic_img = np.zeros((image.shape[0], image.shape[1], 3))
+    for i in range(2, image.shape[0] - 2):
+        for j in range(2, image.shape[1] - 2):
+            # generate patches of size 5x5
+            patch = generate_patch(5, image, i, j)
+
+            # 4 patch cases based on pixel location and rggb bayer pattern
+            if i % 2 == 0 and j % 2 == 0:
+                # both even, green pixel in blue row, predict the red and blue channels
+                demosaic_image[i, j, 0] = np.dot(a_r_gb, patch.flatten())
+                demosaic_img[i, j, 1] = image[i, j]
+                demosaic_img[i, j, 2] = np.dot(a_b_gb, patch.flatten())
+
+            if i % 2 == 1 and j % 2 == 1:
+                # both odd, green pixel in red row, predict the red and blue channels
+                demosaic_image[i, j, 0] = np.dot(a_r_gr, patch.flatten())
+                demosaic_img[i, j, 1] = image[i, j]
+                demosaic_img[i, j, 2] = np.dot(a_b_gr, patch.flatten())
+
+            if i % 2 == 0 and j % 2 == 1:
+                # blue pixel, predict the green and blue channels
+                demosaic_img[i, j, 0] = np.dot(a_r_b, patch.flatten())
+                demosaic_img[i, j, 1] = np.dot(a_g_b, patch.flatten())
+                demosaic_image[i, j, 3] = image[i, j]
+
+            if i % 2 == 1 and j % 2 == 0:
+                # red pixel, predict the green and red channels
+                demosaic_img[i, j, 0] = image[i, j]
+                demosaic_img[i, j, 1] = np.dot(a_g_r, patch.flatten())
+                demosaic_image[i, j, 2] = np.dot(a_b_r, patch.flatten())
+
+    return demosaic_img
+
+
+def load_model_from_csv():
+    """Loads the linear regression model from a csv file.
+    """
+
+    # load model from csv file
+    model = np.loadtxt('model.csv', delimiter=',')
+    return model
 
 
 def main():
     """Main function entry point.
     """
-    image = load_training_images()
-    img_array = np.array(image)
+    training_images = load_training_images()
+    img_array = np.array(training_images)
 
-    # generate a demosaiced image using linear interpolation
-    demosaiced_image = demosaic_linear_interpolation(img_array)
+    # train model
+    model = train_model([img_array])
+
+    # demosaic the image
+    test_image = Image.open('test_images/test1.png')
+    demosaic_img = demosaic_image(test_image, model)
 
     # plot the original image and the filtered image
     fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(image, cmap='gray')
-    ax[1].imshow(demosaiced_image)
+    ax[0].imshow(test_image, cmap='gray')
+    ax[1].imshow(demosaic_image)
     plt.show()
 
     return
